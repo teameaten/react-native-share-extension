5a32244699a2cd44bde55899a5adb18b6aac8330
Steven Carta <scarta@kebabmbp.lan>
2017-06-15 19:19:25 +0100

    Better image support
    -When receiving an image, copy the stream to the app’s local cache, then use a file URL to that file. Better support for random apps (e.g. share from Google Chrome or Lightroom)

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -57,8 +57,49 @@ public class ShareModule extends ReactContextBaseJavaModule {
           value = intent.getStringExtra(Intent.EXTRA_TEXT);
         }
         else if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) ) {
+          String sharedImagePath = null;
           Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-         value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
+          File cacheDir = currentActivity.getCacheDir();
+          File tempFile = new File(cacheDir, "SharedImage");
+          //android.util.Log.i("ReactNativeShare", "Sharing path " + tempFile.toString());
+          tempFile.delete();
+
+          InputStream inStream = null;
+          OutputStream outStream = null;
+          try {
+            inStream = currentActivity.getContentResolver().openInputStream(uri);
+            outStream = new FileOutputStream(tempFile);
+            //android.util.Log.i("ReactNativeShare", "Opened streams");
+            byte[] buf = new byte[4096];
+            int len;
+            while((len=inStream.read(buf))>0){
+                outStream.write(buf,0,len);
+            }
+            sharedImagePath = Uri.fromFile(tempFile).toString();
+            tempFile.deleteOnExit()
+          } catch (FileNotFoundException ex) {
+            sharedImagePath = "file://ERROR/FILENOTFOUND";
+          } catch (IOException ex){
+            sharedImagePath = "file://ERROR/IO";
+          }
+          finally {
+            if (inStream !=null) {
+              try {
+                inStream.close();
+              } catch (IOException ex) {
+                // oh well, eh?
+            }
+            }
+            if (outStream !=null) {
+              try {
+                outStream.close();
+              } catch (IOException ex) {
+                // oh well, eh?
+              }
+            }
+          }
+         //android.util.Log.i("ReactNativeShare", "sharedImagePath is "+ sharedImagePath);
+         value = sharedImagePath;
 
        } else {
          value = "";
